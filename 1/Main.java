7ec6c85d98cd9279c1138dcb608ea8e289a4be09
Connor Farrell <confar5@gmail.com>
2022-12-02 13:07:57 +0100

    Day 1 - Part 2

--- 1/Main.java
+++ 1/Main.java
@@ -3,29 +3,31 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Main {
 
-    public static int solve(List<String> items) {
-        int highest = 0;
+    public static int solve(List<String> items, int numElves) {
+        var highest = new PriorityQueue<Integer>();
         int current = 0;
 
         for (var item : items) {
-            if (item.isEmpty()) {
-                if (current > highest) {
-                    highest = current;
-                }
-                current = 0;
+            if (!item.isEmpty()) {
+                current += Integer.parseInt(item);
                 continue;
             }
-            current += Integer.parseInt(item);
+            highest.add(current);
+            if(highest.size() > numElves){
+                highest.poll();
+            }
+            current = 0;
         }
-        return highest;
+        return highest.stream().reduce(Integer::sum).orElse(0);
     }
 
     public static void main(String[] args) throws IOException {
-        var puzzleInput = Files.readAllLines(Path.of("in.txt"), StandardCharsets.UTF_8);
-        var solution = solve(puzzleInput);
+        var puzzleInput = Files.readAllLines(Path.of("1/in.txt"), StandardCharsets.UTF_8);
+        var solution = solve(puzzleInput,3);
         System.out.println(solution);
     }
 }
