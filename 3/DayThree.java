4f2532bc24d0e21986520556d32b99742668a2ac
Connor Farrell <confar5@gmail.com>
2022-12-03 13:29:44 +0100

    Day 3 - Part 2

--- 3/DayThree.java
+++ 3/DayThree.java
@@ -8,24 +8,32 @@ import java.util.Set;
 
 public class DayThree {
 
-    public static int solve(List<String> rucksacks) {
+    public static int solve(List<String> rucksacks,int elvesPerGroup) {
         int total = 0;
-        for(var rucksack : rucksacks){
-            final Set<Character> firstCompartment = new HashSet<>();
-            final Set<Character> secondCompartment = new HashSet<>();
-
-            for(int i = 0; i < rucksack.length() / 2; i++){
-                firstCompartment.add(rucksack.charAt(i));
+        Set<Character> currentOverlap = new HashSet<>();
+        for(int rucksackIndex = 0; rucksackIndex <= rucksacks.size(); rucksackIndex++){
+            if(rucksackIndex % elvesPerGroup == 0){
+                if(!currentOverlap.isEmpty())
+                    total += itemValue(currentOverlap.stream().findAny().get());
+                if(rucksackIndex != rucksacks.size())
+                    currentOverlap = beginGroup(rucksacks, rucksackIndex);
+                continue;
             }
-            for(int i = rucksack.length() / 2 ; i < rucksack.length(); i++){
-                secondCompartment.add(rucksack.charAt(i));
+            Set<Character> rucksack = new HashSet<>();
+            for(int i = 0; i < rucksacks.get(rucksackIndex).length(); i++){
+                rucksack.add(rucksacks.get(rucksackIndex).charAt(i));
             }
-            firstCompartment.retainAll(secondCompartment);
-            total += itemValue(firstCompartment.stream().findAny().orElseThrow());
-
+            currentOverlap.retainAll(rucksack);
         }
-
         return total;
+    }
+
+    private static Set<Character> beginGroup(List<String> rucksacks, int rucksackIndex) {
+        Set<Character> currentOverlap = new HashSet<>();
+        for(int i = 0; i < rucksacks.get(rucksackIndex).length(); i++){
+            currentOverlap.add(rucksacks.get(rucksackIndex).charAt(i));
+        }
+        return currentOverlap;
     }
 
     private static int itemValue(char item){
@@ -35,7 +43,7 @@ public class DayThree {
 
     public static void main(String[] args) throws IOException {
         var puzzleInput = Files.readAllLines(Path.of("3/in.txt"), StandardCharsets.UTF_8);
-        var solution = solve(puzzleInput);
+        var solution = solve(puzzleInput,3);
         System.out.println(solution);
     }
 }
